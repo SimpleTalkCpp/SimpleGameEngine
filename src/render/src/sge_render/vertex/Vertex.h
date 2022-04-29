91ca5a6e022737a6a0c10f3bec12c1318841401b
SimpleTalkCpp <simpletalkcpp@gmail.com>
2022-04-29 15:18:25 +0200

    VertexType - bug on bitwise

--- src/render/src/sge_render/vertex/Vertex.h
+++ src/render/src/sge_render/vertex/Vertex.h
@@ -36,17 +36,17 @@ struct VertexTypeUtil {
 
 	static constexpr VertexType addNormal(VertexType t, RenderDataType normalType, u8 normalCount) {
 		return static_cast<VertexType>(static_cast<u64>(t) 
-				| (static_cast<u64>(normalType)  << 32) 
-				| (static_cast<u64>(normalCount) << 40));
+				| (static_cast<u64>(normalType)  << 34) 
+				| (static_cast<u64>(normalCount) << 42));
 	}
 
 	static constexpr VertexType addTangent(VertexType t, u8 tangentCount) {
 		return static_cast<VertexType>(static_cast<u64>(t) 
-				| (static_cast<u64>(tangentCount) << 42));
+				| (static_cast<u64>(tangentCount) << 44));
 	}
 	static constexpr VertexType addBinormal(VertexType t, u8 binormalCount) {
 		return static_cast<VertexType>(static_cast<u64>(t) 
-				| (static_cast<u64>(binormalCount) << 44));
+				| (static_cast<u64>(binormalCount) << 46));
 	}
 
 	static constexpr VertexType make(
