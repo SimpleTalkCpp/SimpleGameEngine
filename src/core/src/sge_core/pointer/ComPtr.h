36f79f1c23020a28db94712802d63e6ae8ab4a23
SimpleTalkCpp <simpletalkcpp@gmail.com>
2022-05-01 21:06:21 +0200

    ComPtr, SPtr - bug fix

--- src/core/src/sge_core/pointer/ComPtr.h
+++ src/core/src/sge_core/pointer/ComPtr.h
@@ -8,26 +8,27 @@ public:
 	ComPtr() = default;
 
 	ComPtr(const ComPtr& r)	{ reset(r._p); }
-	ComPtr(ComPtr && r)		{ reset(r._p); r._p = nullptr; }
+	ComPtr(ComPtr && r)		{ _p = r._p; r._p = nullptr; }
 
-	void operator=(const ComPtr& r) { if (r._p == _p) return; reset(r._p); }
-	void operator=(ComPtr && r)		{ if (r._p == _p) return; reset(r._p); r._p = nullptr; }
+	void operator=(const ComPtr& r) { reset(r._p); }
+	void operator=(ComPtr && r)		{ reset(nullptr); _p = r._p; r._p = nullptr; }
 
 	~ComPtr() noexcept { reset(nullptr); }
 
-	T* operator->() noexcept		{ return _p; }
-	operator T*() noexcept			{ return _p; }
+		  T* operator->()       noexcept	{ return _p; }
+	const T* operator->() const noexcept	{ return _p; }
 
-			T* ptr() noexcept		{ return _p; }
-	const	T* ptr() const noexcept	{ return _p; }
+	operator       T*()       noexcept		{ return _p; }
+	operator const T*() const noexcept		{ return _p; }
 
-	void reset(T* p) {
+			T* ptr()       noexcept			{ return _p; }
+	const	T* ptr() const noexcept			{ return _p; }
+
+	void reset(T* p) noexcept {
 		if (p == _p) return;
 		if (_p) {
 			_p->Release();
-			_p = nullptr;
 		}
-
 		_p = p;
 		if (_p) {
 			_p->AddRef();
@@ -36,7 +37,7 @@ public:
 
 	T** ptrForInit() noexcept { reset(nullptr); return &_p; }
 
-	T* detach() { T* o = _p; _p = nullptr; return o; }
+	T* detach() noexcept { T* o = _p; _p = nullptr; return o; }
 private:
 	T* _p = nullptr;
 };
--- src/core/src/sge_core/pointer/SPtr.h
+++ src/core/src/sge_core/pointer/SPtr.h
@@ -8,38 +8,36 @@ public:
 	SPtr() = default;
 
 	SPtr(T* p) noexcept			{ reset(p); }
-	SPtr(SPtr && r) noexcept	{ reset(r._p); r._p = nullptr; }
+	SPtr(SPtr && r) noexcept	{ _p = r._p; r._p = nullptr; }
 
 	void operator=(T* p) noexcept		{ reset(p); }
-	void operator=(SPtr && r) noexcept	{ if (r._p == _p) return; reset(r._p); r._p = nullptr; }
+	void operator=(SPtr && r) noexcept	{ reset(nullptr); _p = r._p; r._p = nullptr; }
 
 	~SPtr() noexcept { reset(nullptr); }
 
-	T* operator->() noexcept		{ return _p; }
-	operator T*() noexcept			{ return _p; }
+		  T* operator->()       noexcept	{ return _p; }
+	const T* operator->() const noexcept	{ return _p; }
 
-			T* ptr() noexcept		{ return _p; }
-	const	T* ptr() const noexcept	{ return _p; }
+	operator       T*()       noexcept		{ return _p; }
+	operator const T*() const noexcept		{ return _p; }
+
+			T* ptr()       noexcept			{ return _p; }
+	const	T* ptr() const noexcept			{ return _p; }
 
 	void reset(T* p) noexcept {
 		static_assert(std::is_base_of<RefCountBase, T>::value, "");
-
 		if (p == _p) return;
 		if (_p) {
 			auto c = --_p->_refCount;
-			if (c <= 0) {
-				sge_delete(_p);
-			}
-			_p = nullptr;
+			if (c <= 0) sge_delete(_p);
 		}
-
 		_p = p;
 		if (_p) {
-			_p->_refCount++;
+			++_p->_refCount;
 		}
 	}
 
-	T* detach() { T* o = _p; _p = nullptr; return o; }
+	T* detach() noexcept { T* o = _p; _p = nullptr; return o; }
 private:
 	T* _p = nullptr;
 };
